342ca48a76b1167f5e4e323ffd43821b09ab479b
pjshin3 <tlsvlfwp1@naver.com>
2020-05-29 07:57:24 +0900

    #2 전화번호 목록 -완료
    -java
    이중포문 안에 startwith를 사용하여 초기값을 서로 비교한다.

--- app/src/main/java/com/example/algorithm/HashTestB.java
+++ app/src/main/java/com/example/algorithm/HashTestB.java
@@ -7,12 +7,12 @@ import java.util.List;
 public class HashTestB {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        List list = new ArrayList();
-        list.addAll(Arrays.asList(phone_book));
-
-
-
-
+        for (int i = 0; i < phone_book.length - 1; i++){
+            for (int l = i+1; l< phone_book.length; l++){
+                if (phone_book[i].startsWith(phone_book[l])) {return false; }
+                if (phone_book[l].startsWith(phone_book[i])) {return false; }
+            }
+        }
         return answer;
     }
 }
