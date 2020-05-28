11b292e51fdaade7fc54ee19793b6a1886b8ca94
pjshin3 <tlsvlfwp1@naver.com>
2020-05-28 22:32:02 +0900

    #2 전화번호 목록 -미완료

---
+++ app/src/main/java/com/example/algorithm/HashTestB.java
@@ -0,0 +1,18 @@
+package com.example.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HashTestB {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        List list = new ArrayList();
+        list.addAll(Arrays.asList(phone_book));
+
+
+
+
+        return answer;
+    }
+}
