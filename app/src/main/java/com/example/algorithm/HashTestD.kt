faf03f08d9b263d3c8622c5460ee16c85a7c08ca
pjshin3 <tlsvlfwp1@naver.com>
2020-07-30 08:07:00 +0900

    1. 베스트앨범 작업 진행중

--- app/src/main/java/com/example/algorithm/HashTestD.kt
+++ app/src/main/java/com/example/algorithm/HashTestD.kt
@@ -4,21 +4,18 @@ class HashTestD {
 
     fun solution(genres: Array<String>, plays: IntArray): IntArray{
         val result = intArrayOf()
-
         val resultMap = mutableMapOf<String,ArrayList<Int>>()
-
         val comparaMap = (0..plays.last()).zip(plays.toList()).toMap()
-        val comparaMapString = (0..genres.size).zip(genres.toList()).toMap()
-
         genres.distinct().forEach {
             resultMap.put(it, arrayListOf())
         }
-
         genres.forEachIndexed { index, s ->
             resultMap[s]?.add(comparaMap.keys.indexOf(index))
         }
 
-        resultMap.forEach {
+        resultMap.values.indices.sorted()
+
+        resultMap.values.forEach {
             println(it)
         }
 
